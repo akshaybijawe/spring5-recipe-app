cd4102adccdac0fde1314d43f15947e1f8c52601
Akshay Bijawe <akshaybijawe121@gmail.com>
2018-04-08 22:26:15 -0700

    Add the Difficulty enum

---
+++ src/main/java/com/akshaybijawe/spring5recipeapp/domain/Difficulty.java
@@ -0,0 +1,10 @@
+package com.akshaybijawe.spring5recipeapp.domain;
+
+/**
+ * @author arbijawe on 4/8/18
+ * @project spring5-recipe-app
+ */
+
+public enum Difficulty {
+    EASY, MODERATE, HARD
+}
